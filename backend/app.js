a5776a5177d46f9787ca411a7be21b51df1e29f9
Ingrid Seurin <ingridseurin@gmail.com>
2024-10-09 09:53:19 +0200

    mise à jour jsonwebtoken et bcrypt

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,14 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"))
 
+//middleware CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization")
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
   next()
 })
-
+//middleware gestion des JSON, fichiers statiques
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "images")))
 app.use("/api/books", booksRoutes)
